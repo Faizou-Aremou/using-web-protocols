7fa4522309a747131f5b565ecc287bceb45f4a92
Faizou <faizouaremou@gmail.com>
2025-06-01 20:35:49 +0200

    - Using Templates with Fastify (2) done - next to do Using Templates with Fastify (3)

--- fastify-web-server/routes/root.js
+++ fastify-web-server/routes/root.js
@@ -1,8 +1,6 @@
 'use strict';
-
 module.exports = async (fastify, opts) => {
   fastify.get('/', async (request, reply) => {
-    const { greeting = 'hello' } = request.query;
-    return reply.view('hello.hbs', { greeting });
+    return reply.view('index.hbs');
   });
 };
